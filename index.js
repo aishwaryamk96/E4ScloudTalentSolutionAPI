d03a849d4fa12c85e5169860552671bd010efb56
Aishwarya M K <aishwarya.mk@techjini.com>
2019-09-23 18:08:07 +0530

    index.js file changes

--- index.js
+++ index.js
@@ -8,40 +8,50 @@ const CREATE = 'create';
 const UPDATE = 'update';
 const DELETE = 'delete';
 
-const processHandler = async() => {
+const processHandler = (event, context) => {
     try {
-        const jobServiceClient = await createAuthCredential();
-        //companyUtils.listCompanies(jobServiceClient);
-        let queue = await data.QueueModel.findAll({
+        createAuthCredential().then(jobServiceClient =>{
+       // to check google key
+       //companyUtils.listCompanies(jobServiceClient);
+       //to check dbb connection
+       data.QueueModel.findAll({
             order: [
-                ['queueId', 'ASC']
+                ['queueId', 'DESC']
             ],
             limit: 1
-        });
-        queue = queue[0];
-        console.log(queue);
-        switch (queue.actionType) {
-            case CREATE:
-                if (queue.serviceType === COMPANY)
-                    companyUtils.createCompany(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
-                else if (queue.serviceType === JOB)
-                    jobUtils.createJob(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
-                break;
-            case UPDATE:
-                if (queue.serviceType === COMPANY)
-                    companyUtils.updateCompany(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
-                else if (queue.serviceType === JOB)
-                    jobUtils.updateJob(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
-                break;
-            case DELETE:
-                if (queue.serviceType === COMPANY)
-                    companyUtils.deleteCompany(jobServiceClient, queue.actionId, queue.queueId);
-                else if (queue.serviceType === JOB)
-                    jobUtils.deleteJob(jobServiceClient, queue.actionId, queue.queueId);
-                break;
-        }
+        }).then(function(queue) {
+       // console.log(queue)
+        queue = queue?queue[0]:[];
+        console.log(queue.queueId);
+       
+       //lambda function
+      // queue = event;
+       switch (queue.actionType) {
+           case CREATE:
+               if (queue.serviceType === COMPANY)
+                   companyUtils.createCompany(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
+               else if (queue.serviceType === JOB)
+                   jobUtils.createJob(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
+               break;
+           case UPDATE:
+               if (queue.serviceType === COMPANY)
+                   companyUtils.updateCompany(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
+               else if (queue.serviceType === JOB)
+                   jobUtils.updateJob(jobServiceClient, queue.payload, queue.actionId, queue.queueId);
+               break;
+           case DELETE:
+               if (queue.serviceType === COMPANY)
+                   companyUtils.deleteCompany(jobServiceClient, queue.actionId, queue.queueId);
+               else if (queue.serviceType === JOB)
+                   jobUtils.deleteJob(jobServiceClient, queue.actionId, queue.queueId);
+               break;
+       }
+   });
+   })
     } catch (err) {
+        console.log(err);
         process.exit();
     }
 };
+//local testing
 processHandler();
